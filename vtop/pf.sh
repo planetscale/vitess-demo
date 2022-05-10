653cfd2193b34eabb90ff9c4e0e1eba48edb3a97
Manan Gupta <manan@planetscale.com>
2022-05-10 16:30:12 +0530

    feat: add port forward and remove 101_initial_cluster from git
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- vtop/pf.sh
+++ vtop/pf.sh
@@ -2,7 +2,7 @@
 
 kubectl port-forward --address localhost "$(kubectl get service --selector="planetscale.com/component=vtctld" -o name | head -n1)" 15000 15999 &
 process_id1=$!
-kubectl port-forward --address localhost "$(kubectl get service --selector="planetscale.com/component=vtgate,!planetscale.com/cell" -o name | head -n1)" 15306:3306 &
+kubectl port-forward --address localhost "$(kubectl get service --selector="planetscale.com/component=vtgate,!planetscale.com/cell" -o name | head -n1)" 15306:3306 15001:15000 &
 process_id2=$!
 sleep 2
 echo "You may point your browser to http://localhost:15000, use the following aliases as shortcuts:"
